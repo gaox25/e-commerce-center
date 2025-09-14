fde8c8ad436e01f25c7f3f824bde284334d86cd6
spitefire <60081445+gaox25@users.noreply.github.com>
2025-09-14 17:54:45 +0800

    将member-service-provider-10000和member-service-consumer-80作为Eureka Client注册到e-commerce-eureka-server-9001上作为服 务提供者

--- member-service-consumer-80/src/main/java/com/gaoxi/springcloud/MemberConsumerApplication.java
+++ member-service-consumer-80/src/main/java/com/gaoxi/springcloud/MemberConsumerApplication.java
@@ -2,7 +2,9 @@ package com.gaoxi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //将该程序标识为Eureka Client
 @SpringBootApplication
 public class MemberConsumerApplication {
     public static void main(String[] args) {
