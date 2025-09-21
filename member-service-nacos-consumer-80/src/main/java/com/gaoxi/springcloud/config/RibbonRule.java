4dee43759df3c62962b4540bc7dd3b4beab94377
spitefire <60081445+gaox25@users.noreply.github.com>
2025-09-21 19:01:53 +0800

    为member-service-nacos-consumber-80添加OpenFeign远程调用，添加缺少的注解

--- member-service-nacos-consumer-80/src/main/java/com/gaoxi/springcloud/config/RibbonRule.java
+++ member-service-nacos-consumer-80/src/main/java/com/gaoxi/springcloud/config/RibbonRule.java
@@ -4,11 +4,11 @@ import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class RibbonRule {
-//    //配置注入自己的负载均衡算法
-//    public IRule myRibbonRule() {
-//        //这里返回的RandomRule，也可以指定其他的负载均衡算法
-//        return new RandomRule();
-//    }
-//}
+@Configuration
+public class RibbonRule {
+    //配置注入自己的负载均衡算法
+    public IRule myRibbonRule() {
+        //这里返回的RandomRule，也可以指定其他的负载均衡算法
+        return new RandomRule();
+    }
+}
